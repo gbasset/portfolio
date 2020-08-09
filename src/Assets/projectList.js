e62539dbe35a3cdd39d90218427db78c97cf020f
gaetan <gbassetprotonmail.com>
2020-08-09 22:25:24 +0200

    add responsive to pages

---
+++ src/Assets/projectList.js
@@ -0,0 +1,35 @@
+import gamePict from './images/game.PNG'
+import movie from './images/movie.png'
+const projectsList = [
+    {
+        key: 'movie',
+        label: 'movie project',
+        picture: movie,
+        link: 'https://moviesearchgaetan.netlify.app/',
+        category: 'react',
+        keywords: 'movies, cinema, react',
+        infos: 'A movie theater nananaa',
+    },
+    {
+        key: 'game pendu',
+        label: 'Javascript game',
+        picture: gamePict,
+        link: 'https://gbasset.github.io/jeux-du-pendu-js-Vanilla/',
+        category: ' Javascript ',
+        keywords: 'movies, cinema, react',
+        infos: 'A javascript game',
+    },
+    {
+        key: '',
+        label: ' ',
+        link: '',
+        category: ' - ',
+        keywords: 'movies, cinema, react',
+        infos: '',
+    },
+
+
+]
+
+
+export default projectsList;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,21 @@
 import React from 'react'
-
+import projectsList from '../../Assets/projectList'
+import './Projects.css'
 export default function Projects() {
     return (
         <div className="containerHight">
             <div className="construction">
                 <h1>Cette page est en construction</h1>
                 <h3>Merci de revenir plus tard</h3>
+                {
+                    projectsList.map(x =>
+                        <div className="project-container">
+                            {x.label}
+                            <img src={x.picture} alt="" />
 
+                        </div>
+                    )
+                }
             </div>
         </div>
     )
