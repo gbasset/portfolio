e11da410437ef2cb7b972f4fe12074518527a18a
gaetan <gbassetprotonmail.com>
2020-08-28 14:42:56 +0200

    add slider and images on cloud

--- src/components/Projects/ProjectItem.js
+++ src/components/Projects/ProjectItem.js
@@ -1,17 +1,34 @@
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
+
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
+
 export default function ProjectItem() {
     const item = useLocation()
     console.log(item);
     const itemN = item.state && item.state.item
     console.log("itemN", itemN);
+    const AutoplaySlider = withAutoplay(AwesomeSlider);
     return (
         <div className="containerHight">
             {itemN &&
                 <>
-                    {itemN.label}
-                    <a href={`${itemN.link}`} target="_blank" rel="noopener noreferrer">Voir</a>
-                    {itemN.Link}
+                    <h1>{itemN.label}</h1>
+                    {itemN.link && <a href={`${itemN.link}`} target="_blank" rel="noopener noreferrer">Voir</a>}
+                    <AutoplaySlider className="slider"
+                        play={true}
+                        cancelOnInteraction={false} // should stop playing on user interaction
+                        interval={4000}
+                    >
+                        {itemN.pictures && itemN.pictures.map(x =>
+                            <div data-src={x} />
+                        )}
+
+                    </AutoplaySlider>
+
+
                 </>
             }
         </div>
